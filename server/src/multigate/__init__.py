2eb41e37ba9eec91d98c9d414b8a734c7774f0cc
Štefan Földesi <stefan.foldesi@gopay.cz>
2024-05-06 12:30:17 +0200

    Remove Vite plugin

--- server/src/multigate/__init__.py
+++ server/src/multigate/__init__.py
@@ -1,21 +1,13 @@
 from pathlib import Path
 
-from jinja2 import Environment, FileSystemLoader
 from litestar import Litestar, get
 from litestar.config.cors import CORSConfig
-from litestar.contrib.jinja import JinjaTemplateEngine
-from litestar.response import Response, Template
-from litestar.template import TemplateConfig
+from litestar.response import Response
 from litestar_granian import GranianPlugin
-from litestar_vite import ViteConfig, VitePlugin
+from litestar.static_files import create_static_files_router
 
 ROOT = Path(__file__).resolve().parent
 FE_ROOT = ROOT / "../../../frontend"
-
-
-@get("/")
-async def index() -> Template:
-    return Template("index.html.j2")
 
 
 @get("/ping")
@@ -23,21 +15,16 @@ async def ping() -> Response:
     return Response({"status": "OK"})
 
 
-# jinja_env = Environment(loader=FileSystemLoader(ROOT / "templates"))
-# template_conf = TemplateConfig(JinjaTemplateEngine.from_environment(jinja_env))
+static_router = create_static_files_router(
+    "static", directories=[FE_ROOT / "dist" / "static"]
+)
 
-vite_conf = ViteConfig(
-    hot_reload=True,
-    use_server_lifespan=True,
-    dev_mode=True,
-    resource_dir=FE_ROOT / "src",
-    template_dir=ROOT / "templates",
-    port=5300,
+html_router = create_static_files_router(
+    "/", directories=[FE_ROOT / "dist"], html_mode=True
 )
 
 app = Litestar(
-    route_handlers=[index, ping],
-    plugins=[GranianPlugin(), VitePlugin(vite_conf)],
+    route_handlers=[ping, html_router, static_router],
+    plugins=[GranianPlugin()],
     cors_config=CORSConfig(allow_origins=["*"]),
-    # template_config=template_conf,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,14 @@ import { defineConfig } from "vite";
 
 import vuetify from "vite-plugin-vuetify";
 
-import litestar from "litestar-vite-plugin";
-
 import path from "path";
 
 const ROOT = path.join(__dirname, "frontend");
-const BE_ROOT = path.join(ROOT, "server", "src", "multigate");
-
-const litestarPlugin = litestar({
-  input: [path.join(ROOT, "src", "main.ts")],
-  resourceDirectory: path.join(__dirname, "src"),
-  hotFile: path.join(ROOT, "public", "hot")
-});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: ROOT,
-  base: "/static/",
-  plugins: [vue(), vuetify(), litestarPlugin],
+  plugins: [vue(), vuetify()],
   envDir: path.resolve(__dirname),
   server: {
     port: 5300,
